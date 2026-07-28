01ab654d834605a2a5e7e94aae7d83f73bde32ef
Jeaven Paras <jeavenanda07@gmail.com>
2026-07-29 02:04:20 +0800

    Add Marquee effect

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,30 @@
+'use client'
+
 import React from 'react'
 import Header from './Header'
 import Image from 'next/image'
 import { ArrowUpRight } from 'lucide-react'
+
+const TAGLINE = 'ASPIRING FRONT-END WEB DEVELOPER'
 
 const Hero = () => {
   return (
     <div className="hero relative flex items-center  h-screen px-10 py-5">
       <div className='relative max-w-[1920px] w-full m-auto h-full flex flex-col justify-center'>
         <Header />
-        
-        <div className='flex relative flex-col w-full items-center justify-center h-full text-center'>
-          <p className='font-extrabold absolute bottom-10 text-[20vh] text-nowrap text-white pointer-events-none'>
-            ASPIRING FRONT-END WEB DEVELOPER
-          </p>
+
+        <div className='flex relative flex-col items-center justify-center h-full text-center  '>
+          <div className='absolute bottom-10 w-dvw overflow-hidden pointer-events-none'>
+            <div className='marquee-track flex whitespace-nowrap'>
+              {/* Two identical copies placed back to back so the loop is seamless */}
+              <span className='font-extrabold text-[20vh] text-white pr-16'>
+                {TAGLINE}
+              </span>
+              <span className='font-extrabold text-[20vh] text-white pr-16'>
+                {TAGLINE}
+              </span>
+            </div>
+          </div>
 
           <button
             aria-label="View more"
